30677632363fe54408650ee2cbf6925b76e7d3b5
Philipp Anné <softyde@annee.de>
2021-12-20 09:19:55 +0100

    new: pinned messages

--- gse-theme.php
+++ gse-theme.php
@@ -2,13 +2,16 @@
 namespace Grav\Theme;
 
 use Grav\Common\Theme;
+use RocketTheme\Toolbox\Event\Event;
 
 class GSETheme extends Theme
 {
     public static function getSubscribedEvents()
     {
         return [
-            'onTwigExtensions' => ['onTwigExtensions', 0]
+            'onTwigExtensions' => ['onTwigExtensions', 0],
+            'onCollectionProcessed' => ['onCollectionProcessed', 10]
+            
         ];
     }
 
@@ -31,4 +34,32 @@ class GSETheme extends Theme
        
        $this->grav['twig']->twig->addFunction($function);        
     }
+    
+    public function onCollectionProcessed(Event $event): void 
+    {
+        /** @var Collection $collection */
+        $collection = $event['collection'];
+
+    $this->grav['debugger']->addMessage($collection->params());
+
+        $use_pinned = $collection->params()['use_pinned'] ?? false;
+        
+        if ((bool) $use_pinned === false) return;
+
+        $removed_items = [];
+
+        foreach ($collection as $item) 
+        {
+            $is_pinned = $item->header()->pinned ?? false;            
+            if ((bool) $is_pinned === false) {
+                $collection->remove($item->path());
+                $removed_items[] = $item;
+            }
+        }
+
+        foreach ($removed_items as $item) 
+        {
+            $collection->addPage($item);
+        }
+    }   
 }
